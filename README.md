f89a1e29da0dd41bb1dde4a1a4b2faaee674df19
BL4CKH47H4CK3R <54761717+BL4CKH47H4CK3R@users.noreply.github.com>
2021-03-04 22:31:32 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,80 @@
+<h1 align=center>Hardened-Anonymized-DNSCrypt-Proxy</h1>
+<p align=center>Wipe Snoopers Out Of Your Networks</p>
+
+
+A flexible DNS proxy, with support for modern encrypted DNS protocols such as [DNSCrypt v2](https://dnscrypt.info/protocol), [DNS-over-HTTPS](https://www.rfc-editor.org/rfc/rfc8484.txt) and [Anonymized DNSCrypt](https://github.com/DNSCrypt/dnscrypt-protocol/blob/master/ANONYMIZED-DNSCRYPT.txt).
+
+
+- ## Features
+
+- For all features please refer to the [OFFICIAL PAGE](https://github.com/DNSCrypt/dnscrypt-proxy#features)
+- All binary files are downloaded from the [OFFICIAL RELEASE PAGE](https://github.com/DNSCrypt/dnscrypt-proxy/releases)
+
+
+- ## Why This Project ?
+
+There Are Automated DNSCrypt-Proxy Client For Both [Windows](https://github.com/bitbeans/SimpleDnsCrypt) & [Android (Magisk Module)](https://git.nixnet.xyz/quindecim/dnscrypt-proxy-android) <br/>
+But For Linux, People Find It Hard To Configure DNSCrypt-Proxy Manually. But I Wanted To Keep It Simple, So It's Here !
+
+
+- ## Supported Linux Distributions
+
+- `Alpine`
+- `Fedora`
+- `Gentoo`
+- `Mageia`
+- `OpenSUSE`
+- `Solus`
+- `Arch / Arch Based Linux`
+- `Debian / Debian Based Linux`
+
+- ## Differences from the main dnscrypt-proxy project
+
+- `server_names` = `acsacsar-ams-ipv4` [NLD], `arvind-io` [IND], `bcn-dnscrypt` [ESP], `d0wn-tz-ns1` [TZA], `dnscrypt.be` [BEL], `dnscrypt.ca-1` [CAN], `dnscrypt.ca-2` [CAN], `dnscrypt.eu-dk` [DNK], `dnscrypt.eu-nl` [NLD], `dnscrypt.one` [DEU], `dnscrypt.pl` [POL], `dnscrypt.uk-ipv4` [GBR], `ev-canada` [CAN], `freetsa.org-ipv4` [CAN] `jp.tiar.app` [JPN], `meganerd` [NLD], `moulticast-ca-ipv4` [CAN], `moulticast-de-ipv4` [DEU], `moulticast-fr-ipv4` [FRA], `moulticast-sg-ipv4` [SGP], `moulticast-uk-ipv4` [GBR], `plan9-dns` [USA], `publicarray-au` [AUS], `pwoss.org-dnscrypt` [DEU], `sarpel-dns-istanbul` [TUR], `scaleway-ams` [NLD], `scaleway-fr` [FRA], `serbica` [NLD], `v.dnscrypt.uk-ipv4` [GBR], `ventricle.us` [USA] are the resolvers in use.
+
+- `doh_servers` = `false` (disable servers implementing the `DNS-over-HTTPS` protocol)
+
+- `require_dnssec` = `true` (server must support `DNSSEC` security extension)
+
+- `timeout` = `1000` (set the max. response time of a single DNS query from `5000` to `1000` ms.)
+
+- `blocked_query_response` = `'refused'` (set `refused` response to blocked queries)
+
+- `dnscrypt_ephemeral_keys` = `true` (create a new, unique key for every single DNS query)
+
+- `fallback_resolvers` = `['91.239.100.100:53']` (use [UncensoredDNS](https://blog.uncensoreddns.org/) instead [CloudFlare](https://iscloudflaresafeyet.com/))
+
+- `netprobe_address` = `'91.239.100.100:53'` (use [UncensoredDNS](https://blog.uncensoreddns.org/) instead [CloudFlare](https://iscloudflaresafeyet.com/))
+
+- `block_ipv6` = `true` (immediately respond to IPv6-related queries with an empty response)
+
+- `blocked_names_file`, `blocked_ips_file`, `allowed_names_file` and `allowed_ips_file` options enabled. (you can now filter your web content, to know how, please refer to the [official documentation](https://github.com/DNSCrypt/dnscrypt-proxy/wiki/Filters) or take a look at my [block repository](https://git.nixnet.services/quindecim/block))
+
+- `anonymized_dns` feature enabled. (`routes` are indirect ways to reach DNSCrypt servers, each resolver has 2 relays assigned)
+
+- `skip_incompatible` = `true` (skip resolvers incompatible with anonymization instead of using them directly)
+
+- `direct_cert_fallback` = `false` (prevent direct connections through the resolvers for failed certificate retrieved via relay)
+
+
+- ## Configure/Deconfigure [Copy-Paste]
+```
+git clone https://github.com/BL4CKH47H4CK3R/Hardened-Anonymized-DNSCrypt-Proxy.git && cd "$(basename "$_" .git)"
+chmod +x run.sh && sudo ./run.sh
+```
+
+- ## DNS Leak Testing [Websites]
+- [BrowserLeaks](https://anon.to/?http://browserleaks.com/dns)
+- [IPLeak](https://anon.to/?http://ipleak.net)
+- [DNSLeakTest](https://anon.to/?https://www.dnsleaktest.com)
+
+### Configuration (post-installing)
+
+- You can edit `dnscrypt-proxy.toml` as you wish located on `/etc/dnscrypt-proxy/dnscrypt-proxy.toml`.
+- For more detailed configuration please refer to [official documentation](https://github.com/DNSCrypt/dnscrypt-proxy/wiki/Configuration).
+
+## Credits
+
+- ## All Credit Goes To -
+[Frank Denis](https://github.com/jedisct1) & All Other Contributors
+For This Awesome [Project](https://github.com/DNSCrypt/dnscrypt-proxy) !
---
+++ run.sh
@@ -0,0 +1,295 @@
+#!/bin/sh
+
+title()
+{
+	echo -e "--------------------------------------------"
+	echo -e "|    Hardened-Anonymized-DNSCrypt-Proxy    |"
+	echo -e "|    Wipe Snoopers Out Of Your Networks    |"
+	echo -e "|         Creator : BL4CKH47H4CK3R         |"
+	echo -e "--------------------------------------------"
+}
+
+gateway=$(ip r | head -1 | cut -c 13- | cut -c -12)
+	
+if [[ ${UID} != 0 ]]
+then
+	clear && title
+	echo -e "----------------------------"
+	echo -e "|  Run The Script As Root  |"
+	echo -e "----------------------------"
+else
+	clear && title
+	echo -e "-------------------------"
+	echo -e "|   [1] Configure       |"
+	echo -e "|   [2] Deconfigure     |"
+	echo -e "|   [3] Configuration   |"
+	echo -e "-------------------------"
+	read -p "[*] Enter Choice [1, 2, 3]: " input
+
+	if [[ ${input} == 1 ]]
+	then
+		clear && title
+		echo -e "-----------------------------------"
+		echo -e "|  Installing DNSCrypt-Proxy ...  |"
+		echo -e "-----------------------------------"
+		
+		if ! [[ -z `which pacman 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "-------------------------------------"
+			echo -e "|  Detected OS : Arch / Arch Based  |"
+			echo -e "-------------------------------------"
+			pacman -Sy dnscrypt-proxy --needed
+
+		elif ! [[ -z `which apt 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "-----------------------------------------"
+			echo -e "|  Detected OS : Debian / Debian Based  |"
+			echo -e "-----------------------------------------"
+			curl -O http://ftp.debian.org/debian/pool/main/d/dnscrypt-proxy/dnscrypt-proxy_2.0.45+ds1-1_amd64.deb
+			apt install dnscrypt-proxy_2.0.45+ds1-1_amd64.deb && rm -rf dnscrypt-proxy_2.0.45+ds1-1_amd64.deb
+
+		elif ! [[ -z `which emerge 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "--------------------------"
+			echo -e "|  Detected OS : Gentoo  |"
+			echo -e "--------------------------"
+			emerge -av dnscrypt-proxy
+
+		elif ! [[ -z `which apk 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "--------------------------"
+			echo -e "|  Detected OS : Alpine  |"
+			echo -e "--------------------------"
+			curl -O http://dl-cdn.alpinelinux.org/alpine/edge/community/x86_64/dnscrypt-proxy-2.0.45-r0.apk
+			apk add --allow-untrusted dnscrypt-proxy-2.0.45-r0.apk && rm -rf dnscrypt-proxy-2.0.45-r0.apk
+		
+		elif ! [[ -z `which dnf 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "--------------------------"
+			echo -e "|  Detected OS : Fedora  |"
+			echo -e "--------------------------"
+			dnf install dnscrypt-proxy
+
+		elif ! [[ -z `which urpmi 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "--------------------------"
+			echo -e "|  Detected OS : Mageia  |"
+			echo -e "--------------------------"
+			urpmi dnscrypt-proxy
+
+		elif ! [[ -z `which zypper 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "----------------------------"
+			echo -e "|  Detected OS : OpenSUSE  |"
+			echo -e "----------------------------"
+			zypper in dnscrypt-proxy
+
+		elif ! [[ -z `which eopkg 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "-------------------------"
+			echo -e "|  Detected OS : Solus  |"
+			echo -e "-------------------------"
+			eopkg install dnscrypt-proxy
+		fi
+
+		echo -e "--------------------------------------"
+		echo -e "|    Disabling SystemD-Resolved ...  |"
+		echo -e "--------------------------------------"
+		systemctl disable --now systemd-resolved -f
+
+		echo -e "---------------------------------------------------------------------"
+		echo -e "|   Applying Hardened-Anonymized-DNSCrypt-Proxy Configurations ...  |"
+		echo -e "---------------------------------------------------------------------"
+		cp -rf allowed-ips.txt /etc/dnscrypt-proxy
+		cp -rf blocked-ips.txt /etc/dnscrypt-proxy
+		cp -rf allowed-names.txt /etc/dnscrypt-proxy
+		cp -rf blocked-names.txt /etc/dnscrypt-proxy
+		cp -rf dnscrypt-proxy.toml /etc/dnscrypt-proxy
+		
+		echo -e "------------------------------------------------------------"
+		echo -e "|   Initializing Hardened-Anonymized-DNSCrypt-Proxy ...    |"
+		echo -e "------------------------------------------------------------"
+		systemctl enable --now dnscrypt-proxy.socket dnscrypt-proxy.service -f
+		dnscrypt-proxy -service start
+		
+		echo -e "----------------------------------------------------"
+		echo -e "|   Configuring & Restarting NetworkManager . . .  |"
+		echo -e "----------------------------------------------------"
+		rm -rf /etc/resolv.conf /etc/resolv.conf.bak
+		rm -rf /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf.bak
+		echo -e "[device]\nwifi.scan-rand-mac-address=yes" >> /etc/NetworkManager/NetworkManager.conf
+		echo -e "ethernet.cloned-mac-address=random" >> /etc/NetworkManager/NetworkManager.conf
+		echo -e "wifi.cloned-mac-address=random\n" >> /etc/NetworkManager/NetworkManager.conf
+		echo -e "[main]\ndns=none" >> /etc/NetworkManager/NetworkManager.conf
+		echo -e "nameserver 0.0.0.0\noptions edns0 single-request-reopen" > /etc/resolv.conf
+		systemctl restart --now NetworkManager -f
+		
+		echo -e "-----------------------------------------"
+		echo -e "|   Hardened-Anonymized-DNSCrypt-Proxy  |"
+		echo -e "|       Successfully Configured !       |"
+		echo -e "-----------------------------------------"
+
+	elif [[ ${input} == 2 ]]
+	then
+		clear && title
+		echo -e
+		echo -e "-------------------------------------"
+		echo -e "|  Uninstalling DNSCrypt-Proxy ...  |"
+		echo -e "-------------------------------------"
+
+		if ! [[ -z `which pacman 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "-------------------------------------"
+			echo -e "|  Detected OS : Arch / Arch Based  |"
+			echo -e "-------------------------------------"
+			pacman -Rcnsu dnscrypt-proxy
+
+		elif ! [[ -z `which apt 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "-----------------------------------------"
+			echo -e "|  Detected OS : Debian / Debian Based  |"
+			echo -e "-----------------------------------------"
+			apt purge dnscrypt-proxy
+
+		elif ! [[ -z `which emerge 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "--------------------------"
+			echo -e "|  Detected OS : Gentoo  |"
+			echo -e "--------------------------"
+			emerge -Cav dnscrypt-proxy
+
+		elif ! [[ -z `which apk 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "--------------------------"
+			echo -e "|  Detected OS : Alpine  |"
+			echo -e "--------------------------"
+			apk del dnscrypt-proxy
+
+		elif ! [[ -z `which dnf 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "--------------------------"
+			echo -e "|  Detected OS : Fedora  |"
+			echo -e "--------------------------"
+			dnf remove dnscrypt-proxy
+
+		elif ! [[ -z `which urpmi 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "--------------------------"
+			echo -e "|  Detected OS : Mageia  |"
+			echo -e "--------------------------"
+			urpme dnscrypt-proxy
+
+		elif ! [[ -z `which zypper 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "----------------------------"
+			echo -e "|  Detected OS : OpenSUSE  |"
+			echo -e "----------------------------"
+			zypper rm dnscrypt-proxy
+
+		elif ! [[ -z `which eopkg 2> /dev/null` && `nmcli networking` = "enabled" ]]
+		then
+			echo -e "-------------------------"
+			echo -e "|  Detected OS : Solus  |"
+			echo -e "-------------------------"
+			eopkg remove dnscrypt-proxy
+		fi
+
+		echo -e "------------------------------------------------------"
+		echo -e "|  Disabling Hardened-Anonymized-DNSCrypt-Proxy ...  |"
+		echo -e "------------------------------------------------------"
+		systemctl disable --now dnscrypt-proxy.socket dnscrypt-proxy.service -f
+		
+		echo -e "---------------------------------------------------------------------"
+		echo -e "|  Reverting Hardened-Anonymized-DNSCrypt-Proxy Configurations ...  |"
+		echo -e "---------------------------------------------------------------------"
+		rm -rf /etc/dnscrypt-proxy
+
+		echo -e "-----------------------------------"
+		echo -e "|  Enabling SystemD-Resolved ...  |"
+		echo -e "-----------------------------------"		
+		systemctl enable --now systemd-resolved -f
+
+		echo -e "---------------------------------------------"
+		echo -e "|  Configuring & Restarting NetworkManager  |"
+		echo -e "---------------------------------------------"
+		rm -rf /etc/resolv.conf /etc/resolv.conf.bak
+		rm -rf /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf.bak		
+		echo -e "[device]\nwifi.scan-rand-mac-address=yes\n" >> /etc/NetworkManager/NetworkManager.conf
+		echo -e "ethernet.cloned-mac-address=random\n" >> /etc/NetworkManager/NetworkManager.conf
+		echo -e "wifi.cloned-mac-address=random" >> /etc/NetworkManager/NetworkManager.conf
+		echo -e "nameserver ${gateway}" > /etc/resolv.conf && systemctl restart --now NetworkManager -f
+
+		echo -e "-----------------------------------------"
+		echo -e "|   Hardened-Anonymized-DNSCrypt-Proxy  |"
+		echo -e "|      Successfully Deconfigured !      |"
+		echo -e "-----------------------------------------"
+		
+	elif [[ ${input} == 3 ]]
+	then
+		clear && title
+		echo -e "---------------------------"
+		echo -e "|   [1] Check Service     |"
+		echo -e "|   [2] Enable Service    |"
+		echo -e "|   [3] Disable Service   |"
+		echo -e "|   [4] Restart Service   |"
+		echo -e "---------------------------"
+		read -p "[*] Enter Choice [1, 2, 3, 4]: " input
+
+		if [[ ${input} == 1 ]]
+		then
+			clear && title
+			echo -e "------------------------------------------------------------------------"
+			echo -e "|   Checking Hardened-Anonymized-DNSCrypt-Proxy Service Status . . .   |"
+			echo -e "------------------------------------------------------------------------"
+			dnscrypt-proxy -show-certs
+
+			echo -e "-----------------------------------------"
+			echo -e "|   Hardened-Anonymized-DNSCrypt-Proxy  |"
+			echo -e "|         Successfully Checked !        |"
+			echo -e "-----------------------------------------"
+
+		elif [[ ${input} == 2 ]]
+		then
+			clear && title
+			systemctl disable --now systemd-resolved -f
+			systemctl enable --now dnscrypt-proxy.socket dnscrypt-proxy.service -f
+			dnscrypt-proxy -service start
+			rm -rf /etc/resolv.conf
+			echo -e "nameserver 0.0.0.0\noptions edns0 single-request-reopen" > /etc/resolv.conf
+			systemctl restart --now NetworkManager -f
+			
+			echo -e "-----------------------------------------"
+			echo -e "|   Hardened-Anonymized-DNSCrypt-Proxy  |"
+			echo -e "|         Successfully Enabled !        |"
+			echo -e "-----------------------------------------"
+			
+		elif [[ ${input} == 3 ]]
+		then
+			clear && title
+			systemctl disable --now dnscrypt-proxy.socket dnscrypt-proxy.service -f
+			systemctl enable --now systemd-resolved -f
+			dnscrypt-proxy -service stop
+			rm -rf /etc/resolv.conf
+			echo -e "nameserver ${gateway}" > /etc/resolv.conf
+			systemctl restart --now NetworkManager -f
+			
+			echo -e "-----------------------------------------"
+			echo -e "|   Hardened-Anonymized-DNSCrypt-Proxy  |"
+			echo -e "|        Successfully Disabled !        |"
+			echo -e "-----------------------------------------"
+			
+		elif [[ ${input} == 4 ]]
+		then
+			clear && title
+			dnscrypt-proxy -service restart
+			rm -rf /etc/resolv.conf
+			echo -e "nameserver 0.0.0.0\noptions edns0 single-request-reopen" > /etc/resolv.conf
+			systemctl restart --now NetworkManager -f
+						
+			echo -e "-----------------------------------------"
+			echo -e "|   Hardened-Anonymized-DNSCrypt-Proxy  |"
+			echo -e "|        Successfully Restarted !       |"
+			echo -e "-----------------------------------------"
+		fi
+	fi
+fi
